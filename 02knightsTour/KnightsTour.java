bedd4c575b3bbbf1efcd106b4ec5475ba51eb05d
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-02-23 13:18:09 -0500

    Finish knightsTour.

--- 02knightsTour/KnightsTour.java
+++ 02knightsTour/KnightsTour.java
@@ -2,6 +2,7 @@ import java.util.*;
 import java.io.*;
 
 
+
 public class KnightsTour{
     //constants for the class
     //terminal specific character to clear screen , or hide/show cursor
@@ -10,7 +11,7 @@ public class KnightsTour{
     final static String show =  "\033[?25h";
     
     //instance variable
-    private char[][]board;
+    private int[][]board;
     private int startx,starty;
     
     //terminal specific character to move the cursor
@@ -29,23 +30,60 @@ public class KnightsTour{
     public String toString(){
 	String ans = "\n";
 	//build your knights tour here...
-	return hide + go(0,0) + ans + "\n" + show;
+	for(int i=0;i<board.length;i++){
+	    for(int z=0;z<board.length;z++){
+		ans+="" + board[z][i]+"\t";
+	    }
+	    ans+="\n";
+	}
+	return hide + clear + go(0,0) + ans + "\n" + show;
     }
 
     public KnightsTour(int size){
+	this(size, 0, 0);
+    }
 
+    public KnightsTour(int size, int startx, int starty){
+	if(size < 1) throw new IllegalArgumentException("The board size must be larger than 0");
+	board = new int[size][size];
+	for(int i=0;i<size;i++){
+	    for(int z=0;z<size;z++){
+		board[i][z] = 0;
+	    }
+	}
+	this.startx = startx;
+	this.starty = starty;
     }
 
 
-    public void solve(){
-	
+    public boolean solve(){
+	//maze[0][0] = 0;
+	return solve(startx,starty,1);
+    }
+
+
+    public boolean solve(int startx, int starty){
+	//maze[startx][starty] = 0;
+	return solve(startx, starty, 1);
     }
 
 
     public boolean solve(int x,int y,int currentMoveNumber){
-	System.out.println(this);
-	wait(20);
-
+	//System.out.println(this);
+	//wait(20);
+	if(x>=board.length || y>=board.length || x<0 || y<0)
+	    return false;
+	if(board[x][y] == board.length * board.length-1)
+	    return true;
+	if(board[x][y] > 0)
+	    return false;
+	if(board[x][y] < 1)
+	    board[x][y] = currentMoveNumber;
+	if(solve(x+2,y+1,currentMoveNumber+1) || solve(x+2,y-1,currentMoveNumber+1) || solve(x+1,y+2,currentMoveNumber+1) ||
+	   solve(x-1,y+2,currentMoveNumber+1) || solve(x-2,y+1,currentMoveNumber+1) || solve(x-2,y-1,currentMoveNumber+1) ||
+	   solve(x+1,y-2,currentMoveNumber+1) || solve(x-1,y-2,currentMoveNumber+1))
+	    return true;
+	board[x][y] = 0;
 	return false;
     }
 
---
+++ 02knightsTour/driver.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+
+public class driver{
+    public static void main(String[]args){
+	KnightsTour board;
+	if(args.length < 1){
+	    System.out.println("Usage:\n\tint boardSize [int startPosx, intstartPosy]");
+	}
+	else{
+	    if(args.length == 1){
+	    board = new KnightsTour(Integer.parseInt(args[0]));
+	    board.solve();
+	    System.out.println(board);
+	    }
+	    else if(args.length == 3){
+	    board = new KnightsTour(Integer.parseInt(args[0]), Integer.parseInt(args[1]),
+				    Integer.parseInt(args[2]));
+	    board.solve();
+	    System.out.println(board);
+	    }
+
+	}
+
+    }
+
+}
