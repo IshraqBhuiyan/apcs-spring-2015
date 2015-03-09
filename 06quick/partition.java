8eecd7b2d8be51c2e3a3911c3c589bb92fef0e60
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-03-09 12:21:07 -0400

    finish quickselect v1

--- 06quick/partition.java
+++ 06quick/partition.java
@@ -11,7 +11,7 @@ public class partition{
     public static void partition(int[] split, int si, int ei){
 	int[] D = new int[split.length];
 	int i =0;
-	for(int i=0;i<si;i++){
+	for(i=0;i<si;i++){
 	    if(i<si || i>ei) D[i] = split[i];
 	}
 	int pivot = split[(Math.random() * (ei - si + 1)) + si];
@@ -29,5 +29,34 @@ public class partition{
 	D[si] = split[si];
     }
 
-    public static void quickselect(int[] arr, int k){ //kth smallest element
-	
+    public static int part(int[] arry, int si, int ei){ //revision of the partition function
+	int[] copy = int[arry.length];
+	int i;
+	for(i=0;i<copy.length;i++){
+	    if(i<si || i>ei) copy[i] = arry[i];
+	}
+	int pivot = arry[(Math.random * (ei - si + 1)) +si]; //randomly determine the pivot in the range of ei to si
+	int end = ei;
+	for(i=si;i<end+1;i++){
+	    if(arry[i] > pivot){
+		copy[ei] = arry[i];
+		ei--;
+	    }
+	    if(arry[i] < pivot){
+		copy[si] = arry[i];
+		si++;
+	    }
+	}
+	copy[si] = pivot;
+	for(i=0;i<copy.length;i++){
+	    arry[i] = copy[i];
+	}
+	return si;
+    }
+    public static int quickselect(int[] arr, int n){ //nth smallest element
+	if(n > arr.length || n < 0) return -1;
+	int start = 0;
+	int end = arr.length;
+	while(part(arr, start, end) != n){};
+	return arr[n];
+    }
