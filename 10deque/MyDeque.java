59d4a9e4fbe726d8fce4a0475eee835cf784eb05
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-04-15 13:07:14 -0400

    Fix something.

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -3,7 +3,8 @@ import java.io.*;
 
 public class MyDeque<T>{
     private Object[] deque;
-    private int start, end;
+    private int start = -1;
+    private int end = -1;
 
     public MyDeque(){
 	deque = new Object[15];
@@ -42,17 +43,34 @@ public class MyDeque<T>{
     }
 
     public T removeFirst(){
-	if(deque[start] == null)
+	if(start == -1 || deque[start] == null)
 	    throw new NoSuchElementException();
 	T item = (T) deque[start];
 	deque[start] = null;
 	if(start + 1 >= deque.length && deque[0] != null){
-	    start++;
+	    start = 0;
 	}else if(deque[start + 1] != null){
 	    start++;
 	}else{
-	    start = null;
+	    start = -1;
 	}
+	return item;
+    }
+
+    public T removeLast(){
+	if(end == -1 || deque[start] == null){
+	    throw new NoSuchElementException();
+	}
+	T item = (T) deque[end];
+	deque[end] = null;
+	if(end - 1 < 0 && deque[deque.length - 1] != null){
+	    end = deque.length - 1;
+	}else if(deque[end - 1] != null){
+	    end--;
+	}else{
+	    end = -1;
+	}
+	return item;
     }
 
     public void resize(){
@@ -66,10 +84,16 @@ public class MyDeque<T>{
     }
 
     public T getFirst(){
+	if(deque[start] == null){
+	    throw new NoSuchElementException();
+	}
 	return (T)deque[start];
     }
 
     public T getLast(){
+	if(deque[end] == null){
+	    throw new NoSuchElementException();
+	}
 	return (T)deque[end];
     }
 
