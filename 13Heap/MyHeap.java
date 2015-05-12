c994f36a7a1761e48e45c1edaa0bfd1f578c1430
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-05-12 13:23:45 -0400

    Make progress on heap.

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -3,11 +3,16 @@ import java.io.*;
 
 public class MyHeap{
     private int[] heap;
+    boolean type; //true is maxheap, false is minheap
 
     public MyHeap(){
+	this(true);
     }
 
     public MyHeap(boolean isMax){
+	type = ismax;
+	heap = new int[15];
+	heap[0] = 0;
     }
 
     public String toString(){
@@ -17,9 +22,25 @@ public class MyHeap{
     }
 
     public void add(int x){
+	if(heap[0] == 0){
+	    heap[1] = x;
+	}else{
+	    if(heap[0] == heap.length-1){
+		resize();
+	    }
+	    if(type){
+		int curr = heap[1];
+		while(true){}
+	    }
+	}
     }
 
     public int peek(){
+	try{
+	    return int[1];
+	}catch(Exception e){
+	    throw new NoSuchElementException("This tree does not have a root.");
+	}
     }
 
 }
