13549168d871aedde55ce388797a859ece22ef05
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-05-12 12:59:10 -0400

    Make progress on heap.

---
+++ 13Heap/MyHeap.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class MyHeap{
+    private int[] heap;
+
+    public MyHeap(){
+    }
+
+    public MyHeap(boolean isMax){
+    }
+
+    public String toString(){
+    }
+
+    public int remove(){
+    }
+
+    public void add(int x){
+    }
+
+    public int peek(){
+    }
+
+}
