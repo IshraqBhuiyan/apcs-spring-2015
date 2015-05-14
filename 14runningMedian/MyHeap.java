d0e053806428f191e4675ada32d3a5a380a75ce7
IshraqBhuiyan <ishraqbhuiyan@gmail.com>
2015-05-14 13:10:02 -0400

    Make progress on RunningMedian.

---
+++ 14runningMedian/MyHeap.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+
+public class MyHeap{
+    private int[] heap;
+    boolean type; //true is maxheap, false is minheap
+
+    public MyHeap(){
+	this(true);
+    }
+
+    public MyHeap(boolean isMax){
+	type = ismax;
+	heap = new int[15];
+	heap[0] = 0;
+    }
+
+    public String toString(){
+	return Arrays.toString(heap);
+    }
+
+    public int remove(){
+	try{
+	    int ind = heap[0];
+	    int temp = heap[ind];
+	    while(ind > 0){
+		temp = heap[ind/2];
+		heap[ind/2] = heap[ind];
+		ind = ind/2;
+	    }
+	    heap[0] = heap[0] - 1;
+	    return temp;
+	}catch(Exception e){
+	    throw new NoSuchElementException("This Heap is empty.");
+	}
+    }
+
+    public void add(int x){
+	if(heap[0] == 0){
+	    heap[1] = x;
+	}else{
+	    if(heap[0] == heap.length-1){
+		resize();
+	    }
+	    int curr = heap[0] + 1;
+	    heap[curr] = x;
+	    if(type){
+		while(curr/2 != 0 && heap[curr/2] >= x){
+		    int temp = heap[curr];
+		    heap[curr] = heap[curr/2];
+		    heap[curr/2] = temp;
+		}
+	    }else{
+		while(curr/2 != 0 && heap[curr/2] <= x){
+		    int temp = heap[curr];
+		    heap[curr] = heap[curr/2];
+		    heap[curr/2] = temp;
+		}
+	    }
+	    heap[0] += 1;
+	}
+    }
+
+    public void resize(){
+	int[] temp = new int[heap.length * 4];
+	for(int i=0;i<heap.length;i++){
+	    temp[i] = heap[i];
+	}
+	heap = temp;
+    }
+
+    public int peek(){
+	try{
+	    return int[1];
+	}catch(Exception e){
+	    throw new NoSuchElementException("This tree does not have a root.");
+	}
+    }
+
+    public int size(){
+	return heap[0];
+    }
+
+}
---
+++ 14runningMedian/RunningMedian.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class RunningMedian{
+    private MyHeap max;
+    private MyHeap min;
+    private int[] median;
+
+    public RunningMedian(){
+	max = new MyHeap(true);
+	min = new MyHeap(false);
+	median = new int[0];
+    }
+
+    public RunningMedian(int val){
+	max = new MyHeap(true);
+	min = new MyHeap(false);
+	median = {val};
+    }
+
+    public void add(int val){
+	if(max.size() == min.size() && median.length < 2){
+	    if(median.length == 0){
+		median = new int[1];
+		median[0] = val;
+	    }else if(median.length == 1){
+		int temp = median[0];
+		median = new int[2];
+		if(val < temp){
+		    median[0] = val;
+		    median[1] = temp;
+		}else{
+		    median[0] = temp;
+		    median[1] = val;
+		}
+	    }
+	}else if(max.size() == min.size()){
+	}
+    }
+}
